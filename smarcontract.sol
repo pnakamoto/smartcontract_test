5098edf387b825388b662be5ab35320563bc5aaa
pnakamoto <140905412+pnakamoto@users.noreply.github.com>
2025-01-18 21:13:25 -0300

    Update smarcontract.sol

--- smarcontract.sol
+++ smarcontract.sol
@@ -43,3 +43,101 @@ contract Storage {
     newNumbers[0] = 12;
 
     return (numbers, newNumbers);
+
+
+
+
+
+// ARRAYS
+
+contract Types {
+
+    uint[6] data;
+
+    function array_exemplo() public returns (uint[6] memory){
+
+        data = [uint(10), 20, 30, 40, 50, 60];
+
+        return (data);
+    }
+}
+
+contract Types {
+
+    uint[6] data;
+
+    function array_exemplo() public returns (uint[6] memory){
+
+        data = [uint(10), 20, 30, 40, 50, 60];
+
+        return (data);
+    }
+}
+
+
+
+// Array dinamico
+
+
+pragma solidity ^0.5.0;
+
+contract Types {
+    int[] data;
+
+    function dynamic_array() public returns (int[] memory) {
+        // Inicializando o array em memória
+        data = new int      data[0] = -60;
+        data[1] = 70;
+        data[2] = -30;
+        data[3] = 90;
+        data[4] = -100;
+        data[5] = -120;
+        data[6] = 140;
+
+        // Criando uma cópia em memória para retornar
+        int[] memory memoryData = data;
+        return memoryData;
+    }
+}
+
+
+// BOOK
+
+pragma solidity ^0.8.0; // Versão mais recente
+
+contract Library {
+    struct Book {
+        string title;
+        string author;
+        uint book_id;
+    }
+
+    Book public book; // Tornar "book" pública para acesso direto
+
+    function setBook() public {
+        book = Book('Solidity for Dummies', 'DIO', 1);
+    }
+
+    function getBookTitle() public view returns (string memory) {
+        return book.title;
+    }
+}
+
+// or 
+
+struct Book {
+    string title;
+    string author;
+    uint book_id;
+}
+
+Book book;
+
+function setBook() public {
+    book = Book('Solidity for Dummies', 'DIO', 1);
+}
+
+function getBookTitle() public view returns (string memory) {
+    return book.title;
+}
+
